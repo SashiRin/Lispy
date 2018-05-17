cc5133f7ef8cc5788b118916b42f46c89c68cb3c
SashiRin <ldy.nero@gmail.com>
2018-05-17 23:17:49 +0900

    Init commit

---
+++ cmake-build-debug/CMakeFiles/Lispy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lispy.dir/main.c.o"
+  "Lispy.pdb"
+  "Lispy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lispy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Lispy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lispy.
+# This may be replaced when dependencies are built.
